2ef16c819419d3ae45a7ef988a4b9f974cd19b20
aandroth <aandroth@gmail.com>
2020-07-17 00:44:29 -0700

    Initial commit to push the gallery

---
+++ Backend_Fns.js
@@ -0,0 +1,17 @@
+//import axios from "https://unpkg.com/axios/dist/axios.min.js"
+
+export function wwwTopFolderInfo() {
+	
+	return ["Game_Files", "Game_Genres.txt"];
+}
+
+export function wwwTopFolderInfo() {
+	
+	return ["Shooter", "Action",               // String[]
+				"Art", "Horror",
+				"Sports", "Puzzle",
+				"ForKids", "StaffFav",
+				"Exploration", "Tutorial",
+				"Simulation", "Movie",
+				"Adventure", "Fantasy"];
+}
